9c6a912a5b09e7a861d9553ecd177cca9d9e2385
taujiong <taujiong@outlook.com>
2020-10-08 01:30:09 +0800

    Account 模块只保留 Web 项目进行页面的修改，回滚其他项目的更改，方便实时跟进官方新功能

--- src/J3space.Abp.Account.Web/AbpAccountWebModule.cs
+++ src/J3space.Abp.Account.Web/AbpAccountWebModule.cs
@@ -1,47 +1,36 @@
-﻿using J3space.Abp.Account.Localization;
-using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Volo.Abp.Account.Localization;
 using Volo.Abp.AspNetCore.Mvc.Localization;
-using Volo.Abp.AutoMapper;
 using Volo.Abp.Modularity;
-using Volo.Abp.UI.Navigation;
 using Volo.Abp.VirtualFileSystem;
 
 namespace J3space.Abp.Account.Web
 {
     [DependsOn(
-        typeof(AbpAccountHttpApiModule),
-        typeof(AbpAutoMapperModule)
+        typeof(Volo.Abp.Account.Web.AbpAccountWebModule)
     )]
     public class AbpAccountWebModule : AbpModule
     {
-        public override void PreConfigureServices(ServiceConfigurationContext context)
-        {
-            context.Services.PreConfigure<AbpMvcDataAnnotationsLocalizationOptions>(options =>
-            {
-                options.AddAssemblyResource(typeof(AbpAccountResource),
-                    typeof(AbpAccountWebModule).Assembly);
-            });
-
-            PreConfigure<IMvcBuilder>(mvcBuilder =>
-            {
-                mvcBuilder.AddApplicationPartIfNotExists(typeof(AbpAccountWebModule).Assembly);
-            });
-        }
-
-        public override void ConfigureServices(ServiceConfigurationContext context)
-        {
-            Configure<AbpVirtualFileSystemOptions>(options =>
-            {
-                options.FileSets.AddEmbedded<AbpAccountWebModule>("J3space.Abp.Account.Web");
-            });
-
-            Configure<AbpNavigationOptions>(options =>
-            {
-                options.MenuContributors.Add(new AbpAccountUserMenuContributor());
-            });
-
-            Configure<RazorPagesOptions>(options => { options.Conventions.AuthorizePage("/Account/Manage"); });
-        }
+        // public override void PreConfigureServices(ServiceConfigurationContext context)
+        // {
+        //     context.Services.PreConfigure<AbpMvcDataAnnotationsLocalizationOptions>(options =>
+        //     {
+        //         options.AddAssemblyResource(typeof(AccountResource),
+        //             typeof(AbpAccountWebModule).Assembly);
+        //     });
+        //
+        //     PreConfigure<IMvcBuilder>(mvcBuilder =>
+        //     {
+        //         mvcBuilder.AddApplicationPartIfNotExists(typeof(AbpAccountWebModule).Assembly);
+        //     });
+        // }
+        //
+        // public override void ConfigureServices(ServiceConfigurationContext context)
+        // {
+        //     Configure<AbpVirtualFileSystemOptions>(options =>
+        //     {
+        //         options.FileSets.AddEmbedded<AbpAccountWebModule>("J3space.Abp.Account.Web");
+        //     });
+        // }
     }
 }
